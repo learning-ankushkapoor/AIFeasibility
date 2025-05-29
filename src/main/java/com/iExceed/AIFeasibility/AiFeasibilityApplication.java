ca04167242b61c0feda54233bcc083dfae609b72
Ankush Kapoor <ankush.kapoor1@i-exceed.com>
2025-05-29 16:57:29 +0530

    first commit

---
+++ src/main/java/com/iExceed/AIFeasibility/AiFeasibilityApplication.java
@@ -0,0 +1,15 @@
+package com.iExceed.AIFeasibility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiFeasibilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiFeasibilityApplication.class, args);
+		
+		System.out.println("Hello");
+	}
+
+}
---
+++ src/test/java/com/iExceed/AIFeasibility/AiFeasibilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iExceed.AIFeasibility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiFeasibilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
